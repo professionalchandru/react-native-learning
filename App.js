5de8ef51afdd259cd67c3a99d6a0d05402f04156
professionalchandru <professionalchandru@gmail.com>
2021-12-07 07:04:51 +0530

    Text Input added. and tested

--- App.js
+++ App.js
@@ -9,15 +9,28 @@
 import React, { useState } from 'react';
 import {
   Text,
+  TextInput,
   View,
 } from 'react-native';
+import { tailwind } from 'tailwind';
 
 const App = () => {
 
-  return (
-    <View>
+  const [name,setName] = useState('')
 
-    </View>
+  return (
+    <>
+      <View style={tailwind('flex-1 bg-white items-center')}>
+        <Text style={tailwind('text-black text-xl m-2.5')}>Please Enter Your Name:</Text>
+        <TextInput onChangeText={(name)=>setName(name)} style={tailwind('border-b border-gray-500 w-3/4 text-center')} />
+
+        <View style={tailwind('my-6')}>
+          <Text style={tailwind('text-blue-500 text-lg italic')}>
+            Your Name Is: {name}
+          </Text>
+        </View>
+      </View>
+    </>
   );
   }
 
